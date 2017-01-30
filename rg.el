6119a1e1653a881d4ac0f3853ea862a8b2ec3bc4
David Landell <david@landells.com>
2017-01-30 12:58:56 +0100

    Bump version to 1.1.0

--- rg.el
+++ rg.el
@@ -6,7 +6,7 @@
 ;;
 ;; Author: David Landell <david.landell@sunnyhill.email>
 ;;         Roland McGrath <roland@gnu.org>
-;; Version: 1.0.0
+;; Version: 1.1.0
 ;; Homepage: https://github.com/davja/rg.el
 ;; Package-Requires: ((cl-lib "0.5") (s "1.10.0"))
 ;; Keywords: matching, tools
