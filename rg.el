3b582d428b23d1a714e9bb95d6e81be594fd60a0
David Landell <david@landells.com>
2018-01-21 21:33:04 +0100

    Bump version to 1.5.0

--- rg.el
+++ rg.el
@@ -6,7 +6,7 @@
 ;;
 ;; Author: David Landell <david.landell@sunnyhill.email>
 ;;         Roland McGrath <roland@gnu.org>
-;; Version: 1.4.1
+;; Version: 1.5.0
 ;; URL: https://github.com/dajva/rg.el
 ;; Package-Requires: ((cl-lib "0.5") (emacs "24") (s "1.10.0"))
 ;; Keywords: matching, tools
