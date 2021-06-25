e09f5e69152785ffc1f9a40c72a9ecc20c68698a
David Landell <david@landells.com>
2021-06-25 11:26:42 +0200

    Bump docs version

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -25,7 +25,7 @@ author = u'David Landell'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'2.0.1'
+release = u'2.1.0'
 
 # -- General configuration ---------------------------------------------------
 
